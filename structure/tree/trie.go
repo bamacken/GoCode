b265eff1a9303de3a8596500f426d25bb7b81397
Bryan Mackenzie <confessionsofpain@gmail.com>
2024-04-25 17:46:28 -0700

    Added Trie / renamed files

--- structure/tree/trie.go
+++ structure/tree/trie.go
@@ -1,54 +1,48 @@
 package tree
 
+// TrieNode represents a node in the trie
 type TrieNode struct {
 	children [26]*TrieNode
 	isEnd    bool
 }
 
-type Trie struct {
-	root *TrieNode
+// NewTrie creates a new TrieNode
+func NewTrie() *TrieNode {
+	return &TrieNode{}
 }
 
-// Constructor
-func (t *Trie) Constructor() *Trie {
-	return &Trie{root: &TrieNode{}}
+// Insert a word into the trie
+func (t *TrieNode) Insert(word string) {
+	node := t
+	for _, c := range word {
+		if node.children[c-'a'] == nil {
+			node.children[c-'a'] = &TrieNode{}
+		}
+		node = node.children[c-'a']
+	}
+	node.isEnd = true
 }
 
-/*
-Define Trie Node Structure:
+// Search a word in the trie
+func (t *TrieNode) Search(word string) bool {
+	node := t
+	for _, c := range word {
+		if node.children[c-'a'] == nil {
+			return false
+		}
+		node = node.children[c-'a']
+	}
+	return node.isEnd
+}
 
-Define a TrieNode class:
-Include a children attribute, which is a dictionary mapping each character to another TrieNode.
-Include an isEndOfWord attribute, a boolean that indicates if the node marks the end of a word.
-Initialize the Trie:
-
-Create the root node:
-Set the root node as a new instance of TrieNode.
-Initialize isEndOfWord to false and children to an empty dictionary.
-Function to Insert a Word:
-
-Start at the root node.
-For each character in the word:
-If the character is not in the current node’s children, add a new TrieNode as its value.
-Move to the TrieNode associated with the character.
-After the last character:
-Set the isEndOfWord of the current node to true.
-Function to Search for a Word:
-
-Start at the root node.
-For each character in the word:
-Check if the character exists in the current node’s children.
-If it does, move to the TrieNode associated with the character.
-If it doesn't, return false because the word is not in the trie.
-After processing all characters:
-Return the value of isEndOfWord at the current node. This will be true if the whole word is present and false if only a prefix is found.
-(Optional) Function to Check for a Prefix:
-
-Start at the root node.
-For each character in the prefix:
-Check if the character exists in the current node’s children.
-If it does, move to the TrieNode associated with the character.
-If it doesn't, return false because the prefix is not present.
-If all characters in the prefix are found:
-Return true indicating the prefix exists in the trie.
-*/
+// Returns if there is any word in the trie that starts with the given prefix
+func (t *TrieNode) StartsWith(prefix string) bool {
+	node := t
+	for _, c := range prefix {
+		if node.children[c-'a'] == nil {
+			return false
+		}
+		node = node.children[c-'a']
+	}
+	return true
+}
